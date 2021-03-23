1bb8f23df9dc831f82f2a3a222b31822ba91decc
Dianaiminza <dianaiminza.99@gmail.com>
2021-03-23 10:11:16 +0300

    first commit

--- test.js
+++ test.js
@@ -1,14 +1,15 @@
+
 const app = require('./index');
 const supertest = require('supertest');
 const request = supertest(app);
 const expect = require("chai").expect;
-
+// const fs = require("fs");
   describe("POST /multiple", function() {
     it("it should has status code 200 if the file is uploaded", function(done) {
      supertest(app)
      .post('/multiple')
-      .attach("file", `${__dirname}\\testfiles\\health.jpg`)
-      .attach("file", `${__dirname}\\testfiles\\health.jpg`)
+      .attach("file", `${__dirname}//testfiles//health.jpg`)
+      .attach("file", `${__dirname}//testfiles//image.png`)
       .expect(200)
       .end(function(err, res){
         if (err) done(err);
@@ -23,8 +24,8 @@ describe("POST /multiple", function() {
    .post('/multiple')
       // .attach("file", "C:\\Users\\Captain\\Pictures\\Saved Pictures\\health.jpg")
       // .attach("file", "C:\\Users\\Captain\\Pictures\\Saved Pictures\\coffee.jpg")
-      .attach("file", `${__dirname}\\testfiles\\health.jpg`)
-      .attach("file", `${__dirname}\\testfiles\\health.jpg`)
+      .attach("file", `${__dirname}//testfiles//health.jpg`)
+      .attach("file", `${__dirname}//testfiles//image.png`)
       .expect(200)
       .end(function(err, res){
         if (err) done(err);
@@ -36,8 +37,8 @@ describe("POST /multiple", function() {
    it("it should has status code 200 if the files are of type  png,jpg,jpeg", function(done) {
     supertest(app)
     .post('/multiple')
-    .attach("file",`${__dirname}\\testfiles\\health.jpg`)
-    .attach("file", `${__dirname}\\testfiles\\health.jpg`)
+    .attach("file",`${__dirname}//testfiles//health.jpg`)
+    .attach("file", `${__dirname}//testfiles//image.png`)
       // .attach("file", "C:\Users\Captain\Documents\CV.docx")
       .expect(200)
       .end(function(err, res){
